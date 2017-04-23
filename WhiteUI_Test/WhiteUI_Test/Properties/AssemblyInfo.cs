fe80e0cbb83b62e33e7f9c51b4c10fe09e8426bf
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-04-23 23:27:58 +0200

    Commit

---
+++ WhiteUI_Test/WhiteUI_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WhiteUI_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhiteUI_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84101fe7-40ca-4922-8e7f-636f771bbb23")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WhiteUI_Test/WhiteUI_Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.MenuItems;
+using TestStack.White.UIItems.WindowStripControls;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.WindowsAPI;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.ListBoxItems;
+
+namespace WhiteUI_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
