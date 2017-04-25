482a915d29916dd41a3c2840cf0efc8264c445b6
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-04-25 23:11:15 +0200

    Commit

--- WhiteUI_Test/WhiteUI_Test/UnitTest1.cs
+++ WhiteUI_Test/WhiteUI_Test/UnitTest1.cs
@@ -33,5 +33,34 @@ namespace WhiteUI_Test
             win.Keyboard.Enter("3");
             win.Keyboard.LeaveKey(KeyboardInput.SpecialKeys.ALT);
         }
+
+        [TestCleanup]
+        public void clineUp()
+        {
+            win.Keyboard.HoldKey(KeyboardInput.SpecialKeys.ALT);
+            win.Keyboard.Enter("1");
+            win.Keyboard.LeaveKey(KeyboardInput.SpecialKeys.ALT);
+            win.Close();
+        }
+
+        [TestMethod]
+        public void TaskOne()
+        {
+            win.Keyboard.HoldKey(KeyboardInput.SpecialKeys.ALT);
+            win.Keyboard.Enter("3");
+            win.Keyboard.LeaveKey(KeyboardInput.SpecialKeys.ALT);
+
+            var expectation = "A";
+            Button button1 = win.Get<Button>(SearchCriteria.ByAutomationId("131"));
+            button1.Click();
+            Button button10 = win.Get<Button>(SearchCriteria.ByAutomationId("130"));
+            button10.Click();
+            RadioButton buttonHex = win.Get<RadioButton>(SearchCriteria.ByAutomationId("313"));
+            buttonHex.Click();
+            Label textBox = win.Get<Label>(SearchCriteria.ByAutomationId("150"));
+            var textcheckBox = textBox.Text;
+
+            Assert.AreEqual(expectation, textcheckBox);
+        }
     }
 }
